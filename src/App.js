ca65b98bc2a078cc949322374ff5fc2cef6c7261
Oleh Makarov <gstancox@gmail.com>
2018-12-02 22:43:15 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FunctionalComp from './components/functionalComp'
 
 class App extends Component {
+  state = {
+    val: 1
+  }
+  componentDidMount =() =>{
+    setInterval(() => {
+      this.setState({val: 1})
+    }, 1000)
+  }
   render() {
+    const {val} = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         <FunctionalComp val={val}></FunctionalComp>
         </header>
       </div>
     );
---
+++ src/components/functionalComp.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default React.memo(({val}) => {
+  return <div>{val}</div>
+})
